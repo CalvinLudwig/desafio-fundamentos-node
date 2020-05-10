878e9764f2c2212d4bf1e98ebd5a90e7c5e0c592
Calvin Ludwig <bludwig.calvin@gmail.com>
2020-05-10 14:38:39 -0300

    final commit

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,31 @@
-import TransactionsRepository from '../repositories/TransactionsRepository';
-import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository'
+import Transaction from '../models/Transaction'
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+  private transactionsRepository: TransactionsRepository
 
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
+  constructor (transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute ({ title, value, type }: Request): Transaction {
+
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance()
+
+      if (total < value) {
+        throw Error('You don\'t have enough balance.')
+      }
+    }
+
+    return this.transactionsRepository.create({ title, value, type })
   }
 }
 
-export default CreateTransactionService;
+export default CreateTransactionService
